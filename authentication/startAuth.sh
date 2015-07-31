49953e9ce930dcf267703df57099e07a2a169a0f
chengchang.wang <chengchang.wang@vipshop.com>
2015-07-31 16:52:57 +0800

    a

--- authentication/startAuth.sh
+++ authentication/startAuth.sh
@@ -1,10 +1,10 @@
 ## get the mongodb service host and port information using envrionement 
 #echo "mongodb.uri = mongodb://$MONGODB_SERVICE_HOST:$MONGODB_SERVICE_PORT/virtual_authentication" >> /apps/config/dev/environment.properties
 
-sed -i 's/\$MONGODB_SERVICE_HOST/\$MONGODB_SERVICE_HOST/g' /apps/config/dev/environment.properties
-sed -i 's/\$MONGODB_SERVICE_PORT/\$MONGODB_SERVICE_PORT/g' /apps/config/dev/environment.properties
-sed -i 's/\$AUTH_SERVICE_HOST/\$AUTH_SERVICE_HOST/g' /apps/config/dev/environment.properties
-sed -i 's/\$AUTH_SERVICE_PORT/\$AUTH_SERVICE_PORT/g' /apps/config/dev/environment.properties
+sed -i "s/\$MONGODB_SERVICE_HOST/$MONGODB_SERVICE_HOST/g" /apps/config/dev/environment.properties
+sed -i "s/\$MONGODB_SERVICE_PORT/$MONGODB_SERVICE_PORT/g" /apps/config/dev/environment.properties
+sed -i "s/\$AUTH_SERVICE_HOST/$AUTH_SERVICE_HOST/g" /apps/config/dev/environment.properties
+sed -i "s/\$AUTH_SERVICE_PORT/$AUTH_SERVICE_PORT/g" /apps/config/dev/environment.properties
 
 
 
